03eaedeb9f2ccee663024bb846e0b38eb3568c40
Lavish <lavish.lavish@triosstudent.com>
2025-06-03 11:04:26 -0400

    code

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	@GetMapping("/welcome")
+
+	public String welcome(){
+
+
+		return "Welcome !";
+	}
+	//echo "# github-actions-example" >> README.md
+	//git init
+	//git add README.md
+	//git commit -m "first commit"
+	//git branch -M main
+	//git remote add origin git@github.com:LavishKamboj01/github-actions-example.git
+	//git push -u origin main
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
